9f0c8b015fe865e08112a7a7d45a21f5b66986a0
Egor Tikhomirov <etikhomirov@1tv.com>
2024-03-15 15:02:17 +0300

    init

---
+++ router.go
@@ -0,0 +1,48 @@
+// Package router фасад над https://github.com/gin-gonic/gin.
+// Сразу предустановлен пакет graceful, gin-contrib/cors,
+package router
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/graceful"
+	"github.com/gin-gonic/gin"
+)
+
+// Router дефолтный Gin роутер с Graceful middleware.
+type Router = *graceful.Graceful
+
+// CorsConfig cors.Config
+type CorsConfig = cors.Config
+
+// Context оболочка над gin.Context.
+type Context = gin.Context
+
+// H оболочка над gin.H.
+type H = gin.H
+
+// HandlerFunc оболочка над gin.HandlerFunc.
+type HandlerFunc = gin.HandlerFunc
+
+// Config конфигурация роутера. Cors - конфигурация корсов, если nil,
+// то будет применен конфиг с "*". Swagger - флаг включения Swagger'а.
+type Config struct {
+	Swagger bool
+}
+
+// New создает инстанс роутера.
+func New(config *Config) (Router, error) {
+	// Создаем дефолтный роутер
+	router, GFErr := graceful.Default()
+
+	// Если не получилось
+	if GFErr != nil {
+		return nil, GFErr
+	}
+
+	return router, nil
+}
+
+// Cors возвращает мидлвару с настройками корсов для роутера
+func Cors(config CorsConfig) HandlerFunc {
+	return cors.New(config)
+}
